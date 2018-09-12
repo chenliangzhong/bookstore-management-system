67288ad13bee270e5c0a4f38eb1773a7122930c1
Syo Pain <syopain@qq.com>
2018-09-12 22:23:29 +0800

    Update Address.java

--- src/main/java/com/bookstore/bean/Address.java
+++ src/main/java/com/bookstore/bean/Address.java
@@ -4,7 +4,7 @@ package com.bookstore.bean;
  * Created by heanxing on 2018/9/11.
  */
 public class Address {
-    private  Long id;
+    private Long id;
     private String name;//收货人名字
     private String area;//地区
     private String detailed;//详细地址
