f7b140be65454c777535d5b06ee4b2a18b64e949
jinhui <2422553065@qq.com>
2018-09-19 17:01:25 +0800

    更新product接口

---
+++ src/main/java/com/bookstore/service/ProductService.java
@@ -0,0 +1,20 @@
+package com.bookstore.service;
+
+import com.bookstore.bean.Product;
+
+import java.util.List;
+
+/**
+ * Created by SX-503 on 2018/9/19.
+ */
+public interface ProductService {
+    int insert(Product product);
+
+    Product selectById(Long id);
+
+    List<Product> select();
+
+    int updateById(Product product);
+
+    int deleteBatch(Long... id);
+}
